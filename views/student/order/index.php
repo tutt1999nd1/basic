9d82d3917b078b001e54bad338bfb33e7896339d
tutt1999nd1 <thanhtu69@gmail.com>
2020-05-27 17:32:33 +0700

    Hoàn thành

---
+++ views/student/order/index.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Sách đã mượn';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="order-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            'id',
+            'book_id',
+            ['attribute' => 'book.name'],
+            'order_date',
+            'expiration_date',
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
